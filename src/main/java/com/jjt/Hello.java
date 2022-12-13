18930fec52f58872f7e6a5ff6544370038065cfb
jiujingtian <jiujingtian@bytedance.com>
2022-12-14 03:46:01 +0800

    添加、修改、删除文件测试

--- src/main/java/com/jjt/Hello.java
+++
@@ -1,7 +0,0 @@
-package com.jjt;
-
-public class Hello {
-    public static void main(String[] args) {
-        System.out.println("这个是添加文件");
-    }
-}
---
+++ src/main/java/com/jjt/Hello2.java
@@ -0,0 +1,7 @@
+package com.jjt;
+
+public class Hello2 {
+    public static void main(String[] args) {
+        System.out.println("添加文件");
+    }
+}
